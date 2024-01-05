e62e733390b68ca8f06fd0ad8d8df07428e8343a
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-01-05 10:58:07 +0000

    feat: added userMedia component

---
+++ app/components/userMedia.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Movie } from "../types/interfaces";
+
+interface UserMediaProps {
+  savedMovies: Movie[];
+}
+
+const UserMedia: React.FC<UserMediaProps> = ({ savedMovies }) => {
+  return (
+    <>
+      {" "}
+      {savedMovies.map((savedMovie) => (
+        <div key={savedMovie.id}>
+          {" "}
+          <h2>{savedMovie.original_title}</h2>
+          <p>Release Date: {savedMovie.release_date}</p>
+          <p>Rating: {savedMovie.vote_average}</p>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${savedMovie.poster_path}`}
+            alt={savedMovie.original_title}
+          />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default UserMedia;
