7b748369c8fc2d93993272654713ccd7eda987ff
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-01-05 11:01:25 +0000

    feat: added types

---
+++ app/types/global.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  namespace NodeJS {
+    interface Global {
+      mongoose: {
+        conn: typeof import("mongoose") | null;
+        promise: Promise<typeof import("mongoose")> | null;
+      };
+    }
+  }
+}
---
+++ app/types/interfaces.ts
@@ -0,0 +1,7 @@
+export interface Movie {
+  original_title: string;
+  release_date: string;
+  vote_average: string;
+  id: string;
+  poster_path: string;
+}
