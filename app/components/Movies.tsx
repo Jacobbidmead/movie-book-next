a5a8e0506a586499daead335aa4186693af09fc5
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-01-08 14:14:28 +0000

    refactor: created Movie component and passed props
    All of the movies searched are now in Movie component, this allows me to create a Shows component and lets the user switch between the two

---
+++ app/components/Movies.tsx
@@ -0,0 +1,58 @@
+import { Movie } from "../types/interfaces";
+
+interface MovieProps {
+  movies: Movie[];
+  addMovie: (movieToSave: Movie) => void;
+  handleAddToList: (movieId: string) => void;
+  addedMovies: { [key: string]: boolean };
+}
+
+const Movies: React.FC<MovieProps> = ({
+  movies,
+  addMovie,
+  addedMovies,
+  handleAddToList,
+}) => {
+  return (
+    <div className="grid grid-cols-6">
+      {movies.map((movie, i) => (
+        <div className="flex flex-col p-2" key={i}>
+          <div className="tooltip">
+            <h2 className="truncate">
+              {movie.original_title.length > 20
+                ? `${movie.original_title.substring(0, 20)}...`
+                : movie.original_title}
+            </h2>
+            {movie.original_title.length > 20 && (
+              <div className="tooltiptext">{movie.original_title}</div>
+            )}
+          </div>
+          <p>Rating: {movie.vote_average}</p>
+          <img
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                : "/image.png"
+            }
+            alt={movie.original_title}
+          />
+
+          {addedMovies[movie.id] ? (
+            <div>Saved to list</div>
+          ) : (
+            <button
+              onClick={() => {
+                addMovie(movie);
+                handleAddToList(movie.id);
+              }}
+            >
+              Save
+            </button>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Movies;
--- app/pages/media.tsx
+++ app/pages/media.tsx
@@ -6,6 +6,7 @@ import { useFetchMovies } from "../hooks/useFetchMovies";
 import { useFetchShows } from "../hooks/useFetchShows";
 import UserMedia from "../components/userMedia";
 import { Movie } from "../types/interfaces";
+import Movies from "../components/Movies";
 // import LoginPage from "./login.page";
 
 // displays movie search
@@ -84,44 +85,12 @@ const MoviePage: React.FC = () => {
           handleRemoveFromList={handleRemoveFromList}
         />
       ) : (
-        <div className="grid grid-cols-6">
-          {movies.map((movie, i) => (
-            <div className="flex flex-col p-2" key={i}>
-              <div className="tooltip">
-                <h2 className="truncate">
-                  {movie.original_title.length > 20
-                    ? `${movie.original_title.substring(0, 20)}...`
-                    : movie.original_title}
-                </h2>
-                {movie.original_title.length > 20 && (
-                  <div className="tooltiptext">{movie.original_title}</div>
-                )}
-              </div>
-              <p>Rating: {movie.vote_average}</p>
-              <img
-                src={
-                  movie.poster_path
-                    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-                    : "/image.png"
-                }
-                alt={movie.original_title}
-              />
-
-              {addedMovies[movie.id] ? (
-                <div>Saved to list</div>
-              ) : (
-                <button
-                  onClick={() => {
-                    addMovie(movie);
-                    handleAddToList(movie.id);
-                  }}
-                >
-                  Save
-                </button>
-              )}
-            </div>
-          ))}
-        </div>
+        <Movies
+          movies={movies}
+          addMovie={addMovie}
+          handleAddToList={handleAddToList}
+          addedMovies={addedMovies}
+        />
       )}
     </>
   );
