9dc1be4d3be7f971bedcb19453c5d056354476db
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-01-17 13:35:42 +0000

    docs: added comments to media functions
    helps me to track what each function is doing

--- app/pages/media.tsx
+++ app/pages/media.tsx
@@ -27,35 +27,49 @@ const MoviePage: React.FC = () => {
 
   const addMedia = (media: Movie | Show) => {
     setSavedMedia((prevMedia) => {
+      // if the element id (item) is matched an id of the media, save as variable isMediaSaved
       const isMediaSaved = prevMedia.some((item) => item.id === media.id);
+      // if the element id (item) is not === media.id, then we call it updatedMedia
+      // we can return the previous elements (items) and the new item
       if (!isMediaSaved) {
         const updatedMedia = { ...media, type: media.type } as
           | Movie
           | (Show & { type: "movie" | "show" });
         return [...prevMedia, updatedMedia];
       }
+      // if it is === media.id, then we return the previous elements
       return prevMedia;
     });
   };
 
-  // remove media from saved movie list
+  // Function to remove a media item (either a movie or a show) from the saved list.
+
   const removeMedia = (mediaId: string, mediaType: "movie" | "show") => {
+    // Update the 'savedMedia' state.
+    // This state holds an array of all saved media items.
     setSavedMedia((prevMedia) =>
+      // Filter out the media item that matches the given 'mediaId' and 'mediaType'.
+      // This effectively removes the item from the list of saved media.
       prevMedia.filter(
         (media) => !(media.id === mediaId && media.type === mediaType)
       )
     );
+    // Update the 'addedMedia' state.
 
+    // This state is used to track whether each individual media item is saved or not.
+    // It has a structure that differentiates between 'movies' and 'shows'.
     setAddedMedia((prevAdded) => ({
-      ...prevAdded,
+      ...prevAdded, // Spread the existing state to maintain other entries.
+      // Based on the 'mediaType', update either the 'movies' or 'shows' part of the state.
       [mediaType === "movie" ? "movies" : "shows"]: {
-        ...prevAdded[mediaType === "movie" ? "movies" : "shows"],
-        [mediaId]: false,
+        ...prevAdded[mediaType === "movie" ? "movies" : "shows"], // Spread the existing 'movies' or 'shows' state.
+        [mediaId]: false, // Set the specific media item's saved status to false, indicating it is no longer saved.
       },
     }));
   };
 
   // toggles between searched media and saved media
+  // each time the onClick is called, the state toggles between true and false
   const showMedia = () => {
     setShowUserMedia((prevState) => !prevState);
   };
