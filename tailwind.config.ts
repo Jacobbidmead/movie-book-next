61aaa927f861f76bfb8cca8715fe6463f0cfe571
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-01-17 20:08:06 +0000

    style: added daisy UI theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,8 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [require("daisyui")],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+  daisyui: {
+    themes: ["cupcake"],
   },
-  plugins: [],
 };
 export default config;
