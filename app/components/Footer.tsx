46d303f991c9202a866ca4506508a41c6808ddd2
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2024-07-02 14:07:05 +0100

    style: adjusted fitter styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,9 +3,9 @@ const Footer: React.FC = () => {
     <>
       {" "}
       <div className="px-12 py-4  text-sm border-border border-t-[2px] text-light flex justify-end bg-night ">
-        <div className="px-4">
+        <div className="px-4 footer-text">
           MovieBookAI was created by{" "}
-          <a href="https://jacobbidmead.com/" target="_blank">
+          <a href="https://jacobbidmead.com/" target="_blank" className="footer-link">
             jacobbidmead.com
           </a>
         </div>
